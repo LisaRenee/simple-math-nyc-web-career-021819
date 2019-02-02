01b7fbca0dbdb2dab2a2a92ae232fee9b36a0ef5
Lisa Scott <lrscot@aol.com>
2019-02-02 05:12:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,25 +1,25 @@
 def addition(num1, num2)
-  1 + 2
+  5 + 4
 end
 
 def subtraction(num1, num2)
-  3 - 1
+10 - 5
 end
 
 def division(num1, num2)
-  42 / 6
+50 / 2
 end
 
 def multiplication(num1, num2)
-  5 * 5
+  4 * 30
 end
 
 def modulo(num1, num2)
-  7 % 3
+  34 % 5
 end
 
 def square_root(num)
-  Math.sqrt(9)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
