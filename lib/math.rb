c4ce7f398a50b3dc45775486ecc01922e19897ab
Lisa Scott <lrscot@aol.com>
2019-02-02 05:10:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
+  1 + 2
 end
 
 def subtraction(num1, num2)
+  3 - 1
 end
 
 def division(num1, num2)
+  42 / 6
 end
 
 def multiplication(num1, num2)
+  5 * 5
 end
 
 def modulo(num1, num2)
+  7 % 3
 end
 
 def square_root(num)
+  Math.sqrt(9)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  5 + ((3 * 2) /2)
+  #Hint:  __5 + (( _3_ * __2 ) / __ )
 end
